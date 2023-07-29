17a1459c56dd9be7f89fb8aa377f3c01fa299a0d
Parathps7 <77536966+Parathps7@users.noreply.github.com>
2023-07-29 14:03:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const options = {
 		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
 	}
 };
-
+let fin_temp=0;
 const getWeather = (city) => {
     cityName.innerHTML=city
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city,options)
@@ -31,6 +31,36 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city,optio
         wind_degrees.innerHTML=response.wind_degrees
         sunrise.innerHTML=response.sunrise
         sunset.innerHTML=response.sunset
+        
+        console.log(response.temp)
+            if(response.temp > 15)
+            {
+                document.querySelector('#bdy').style.backgroundImage = 'url("/sun.webp")';
+                document.querySelector('#bdy').style.backgroundSize = 'cover';
+            }
+            if(response.cloud_pct >80 )
+            {
+                document.querySelector('#bdy').style.backgroundImage = 'url("/rain.gif")';
+                document.querySelector('#bdy').style.backgroundSize = 'cover';
+            }  
+            if(response.temp <15 && response.temp > 0 )
+            {
+                document.querySelector('#bdy').style.backgroundImage = 'url("/gorgeous-clouds-background-with-blue-sky-design_1017-25501.avif")';
+                document.querySelector('#bdy').style.backgroundSize = 'cover';
+            }  
+            if(response.temp <0 )
+            {
+                document.querySelector('#bdy').style.backgroundImage = 'url("/snow.gif")';
+                document.querySelector('#bdy').style.backgroundSize = 'cover';
+            }  
+            const currentTime = new Date();
+            const currentHour = currentTime.getHours();
+
+            console.log(currentHour);
+            if (currentHour > 19) {
+                document.body.classList.add('time-lt-19');
+              }
+
     })
 .catch(err => console.error(err));
 
@@ -41,3 +71,4 @@ submit.addEventListener("click",(e)=>
     getWeather(city.value)
 })
 getWeather("Jammu")
+
